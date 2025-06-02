f45e57b93a10d8d06f8105ac24a12852fd45792a
baimaoshouerkong <baimaoshouer@Gmail.com>
2025-06-02 21:32:23 +0800

    环境搭建完成

---
+++ src/components/Index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="index">
+    <h1>我叫白毛兽耳控</h1>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Index',
+  setup() {
+    
+  },
+}
+</script>
+
+<style scoped>
+.index{
+  background-color: skyblue;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+</style>
+
